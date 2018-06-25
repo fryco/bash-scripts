5b0974bb31b244d65bae370c9bdac7d87419e0c1
fryco <expcaplab@gmail.com>
2018-06-25 20:50:10 +0200

    Mandatory flag with value validation added

--- parser.sh
+++ parser.sh
@@ -1,18 +1,71 @@
 #!/usr/bin/env bash
 SCRIPT_AUTHOR="John Novak"
-SCRIPT_VERSION=1.0
+SCRIPT_VERSION=1.1
 SCRIPT_DATE=1.1.1970
 SCRIPT_NAME=$(echo ${0##*/})
 
 script_main_func()
 {
+    check_input_args
     echo $example_value
+    echo $example_value2
     echo $example_bool
+}
+
+### If you want to have mandatory arguments add here condition to check if they
+### are properly populated
+check_input_args()
+{
+    ### Each flag, which needs to be verified has to have its own following block
+    ### of code. Copy&paste&adjust it for all necessary 
+    ### Begin block
+    looking_for=$example_value
+    valid_values=(one two three)
+    if [ -n "$looking_for" ]; then
+        is_valid=false
+        for elem in ${valid_values[@]};
+        do
+            if [ "$elem" == "$looking_for" ]; then
+                is_valid=true
+                break
+            fi
+        done
+
+        if  [ $is_valid = false ]; then 
+            echo "ERR: Invalid value detected ($looking_for)"
+            exit 1
+        fi
+    else
+        echo "ERR: Missing mandatory flag: example"
+        exit 1
+    fi
+    ### End block
+    looking_for=$example_value2
+    valid_values=(four five six)
+    if [ -n "$looking_for" ]; then
+        is_valid=false
+        for elem in ${valid_values[@]};
+        do
+            if [ "$elem" == "$looking_for" ]; then
+                is_valid=true
+                break
+            fi
+        done
+
+        if  [ $is_valid = false ]; then 
+            echo "ERR: Invalid value detected ($looking_for)"
+            exit 1
+        fi
+    else
+        echo "ERR: Missing mandatory flag: example2"
+        exit 1
+    fi
+
 }
 
 show_help()
 {
-	echo "### $SCRIPT_NAME user manual"
+    echo "### $SCRIPT_NAME user manual"
     echo
     echo "NAME:"
     echo "  $SCRIPT_NAME"
@@ -36,7 +89,8 @@ show_credentials()
     echo "NAME: $SCRIPT_NAME, VERSION: $SCRIPT_VERSION, AUTHOR: $SCRIPT_AUTHOR, LAST UPDATE: $SCRIPT_DATE"
 }
 
-example_value=''
+example_value=""
+example_value2=""
 example_bool=false
 skip_arg=false
 
@@ -44,17 +98,31 @@ skip_arg=false
 if [[ 0 -eq $# ]]; then
     show_help
 fi
+
 for arg in "$@"; do
     if $skip_arg; then 
         skip_arg=false
         continue
     fi
+
     case "$arg" in
         -ex|--example)
             var_name="${1}"
             shift
             if [[ "$1" != "" ]]; then
                 example_value="${1}"
+                shift
+                skip_arg=true
+            else
+                echo "ERR: value missing for $var_name argument"
+                exit 1
+            fi
+            ;;
+        -ex2|--example2)
+            var_name="${1}"
+            shift
+            if [[ "$1" != "" ]]; then
+                example_value2="${1}"
                 shift
                 skip_arg=true
             else
